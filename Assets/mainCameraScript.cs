4a1237f8a3a3bde1017b36878dfb4e777d940543
rafimichaeli <rafi.michaeli@gmail.com>
2020-02-12 17:11:03 +0200

    initial
    initial

---
+++ Assets/mainCameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TestFairyUnity;
+
+public class mainCameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       TestFairy.begin("cc38070ec3d9684a91879a2ec0e8a21fc67b36ed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
