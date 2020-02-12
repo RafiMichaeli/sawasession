4425bf64d91090383c2c308874032fcfc56bdae1
rafimichaeli <rafi.michaeli@gmail.com>
2020-02-12 17:15:14 +0200

    added script for CB
    script for CB

---
+++ Assets/Editor/TestFairyBuildPostProcessor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class TestFairyBuildPostProcessor {
+  [PostProcessBuildAttribute(1)]
+  public static void OnPostprocessBuild(BuildTarget buildTarget, string path) {
+    if (buildTarget == BuildTarget.iOS) {
+      string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+      PBXProject proj = new PBXProject();
+      proj.ReadFromString(File.ReadAllText(projPath));
+      var mainTarget = proj.GetUnityMainTargetGuid();
+
+      proj.AddShellScriptBuildPhase(mainTarget, "Strip unused architectures", "", "sh Frameworks/Plugins/iOS/TestFairy.framework/strip-architectures.sh");
+
+      File.WriteAllText(projPath, proj.WriteToString());
+    }
+  }
+}
