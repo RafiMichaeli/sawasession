bb275573d6f47847f6ea515cde7282398e536cc4
rafimichaeli <rafi.michaeli@gmail.com>
2020-02-13 14:36:54 +0200

    updated sh script
    update sh script

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -6,7 +6,7 @@ BUILD_TARGET="androidCB"
 curl https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
     -F "ipa=@$2/${BUILD_TARGET}.apk" \
-    -F testers_groups='' \
-    -F notify='on'
-    -F release_notes='testing connection to upload api'
-    -F tags='UnityCB'
+#   -F testers_groups='' \
+#   -F notify='on'
+#   -F release_notes='testing connection to upload api'
+#   -F tags='UnityCB'
