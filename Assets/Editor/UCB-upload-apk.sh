e03f2e3eef351c811b6906c4f76eea84713440c5
rafimichaeli <rafi.michaeli@gmail.com>
2020-02-13 14:58:17 +0200

    ls
    ls

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -3,6 +3,11 @@
 APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
 BUILD_TARGET="androidCB"
 
+echo $1
+echo $2
+env 
+find $2 -ls
+
 curl https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
     -F "ipa=@$2/${BUILD_TARGET}.apk" \
