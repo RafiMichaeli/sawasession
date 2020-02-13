665741d7a2f0e5651aafba751dcfcc48b0040030
rafimichaeli <rafi.michaeli@gmail.com>
2020-02-13 16:20:55 +0200

    comment
    comment

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
-BUILD_TARGET="androidCB"
+BUILD_TARGET="*"
 
-echo $1
-echo $2
-env 
-echo "===="
-find $1 -ls
-echo "====="
-find $2 -ls
-echo "====="
-find . -ls
-echo "====="
-find / -ls
-echo "====="
+# echo $1
+# echo $2
+# env 
+# echo "===="
+# find $1 -ls
+# echo "====="
+# find $2 -ls
+# echo "====="
+# find . -ls
+# echo "====="
+# find / -ls
+# echo "====="
 
 curl https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
