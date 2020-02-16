8059b85013f4b307e13bcd7450551084591e517e
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-02-16 15:30:06 +0200

    Update UCB-upload-apk.sh

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -3,12 +3,16 @@
 APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
 BUILD_TARGET="*"
 
-brew install jq
-curl -X GET -H "Content-Type: application/json"  -H "Authorization: Basic 946d7ecb0c817c4f2c166fba0ec0fe84" "https://build-api.cloud.unity3d.com/api/v1/orgs/testfairy/projects/sawsessions/buildtargets/default-ios/builds?per_page=1&page=1" | jq
+ls -lR /BUILD_PATH
+echo "======"
+find /BUILD_PATH -ls
 
-curl https://upload.testfairy.com/api/upload \
-    -F "api_key=${APP_TOKEN}" \
-    -F "ipa=@$2/${BUILD_TARGET}.apk" \
+#brew install jq
+#curl -X GET -H "Content-Type: application/json"  -H "Authorization: Basic 946d7ecb0c817c4f2c166fba0ec0fe84" "https://build-api.cloud.unity3d.com/api/v1/orgs/testfairy/projects/sawsessions/buildtargets/default-ios/builds?per_page=1&page=1" | jq
+
+#curl https://upload.testfairy.com/api/upload \
+#    -F "api_key=${APP_TOKEN}" \
+#    -F "ipa=@$2/${BUILD_TARGET}.apk" \
 #   -F testers_groups='' \
 #   -F notify='on'
 #   -F release_notes='testing connection to upload api'
