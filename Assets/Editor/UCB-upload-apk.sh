b2b002e4a10b9868d7b7209ff4aa877a9ca78ce4
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-02-16 15:06:22 +0200

    Update UCB-upload-apk.sh

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -6,7 +6,6 @@ BUILD_TARGET="*"
 brew install jq
 curl -X GET -H "Content-Type: application/json"  -H "Authorization: Basic 946d7ecb0c817c4f2c166fba0ec0fe84" "https://build-api.cloud.unity3d.com/api/v1/orgs/testfairy/projects/sawsessions/buildtargets/default-ios/builds?per_page=1&page=1" | jq
 
-
 curl https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
     -F "ipa=@$2/${BUILD_TARGET}.apk" \
