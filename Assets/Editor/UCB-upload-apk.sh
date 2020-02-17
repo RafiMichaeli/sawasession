ad814628d1d9cec1f0c021bfb44423f59a1fabd3
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-02-17 17:20:49 +0200

    Update UCB-upload-apk.sh

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -4,9 +4,9 @@ APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
 
 curl -v https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
-    -F "ipa=@$WORKSPACE/.build/last/default-ios/build.ipa”
+    -F "ipa=@$WORKSPACE/.build/last/default-ios/build.ipa"
 
-#    -F "ipa=@/BUILD_PATH/testfairy.sawsessions.default-ios/.build/last/default-ios/build.ipa"
+#   -F "ipa=@/BUILD_PATH/testfairy.sawsessions.default-ios/.build/last/default-ios/build.ipa"
 #   -F testers_groups='' \
 #   -F notify='on'
 #   -F release_notes='testing connection to upload api'
