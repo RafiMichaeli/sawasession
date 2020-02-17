cc99ca750a73de1d08ff1d3f8fa50ec64ef14b18
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-02-17 16:11:00 +0200

    Update UCB-upload-apk.sh

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -1,22 +1,10 @@
 #!/bin/bash -x
 
 APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
-#BUILD_TARGET="*"
-
-#npm install
-
-#$(npm bin)/firebase --version
-
-#ls -lR /BUILD_PATH
-#echo "======"
-#find /BUILD_PATH -ls
-
-#brew install jq
-#curl -X GET -H "Content-Type: application/json"  -H "Authorization: Basic 946d7ecb0c817c4f2c166fba0ec0fe84" "https://build-api.cloud.unity3d.com/api/v1/orgs/testfairy/projects/sawsessions/buildtargets/default-ios/builds?per_page=1&page=1" | jq
 
 curl -v https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
-    -F "ipa+@/api/v1/orgs/testfairy/projects/sawsessions/buildtargets/default-ios/builds/20/*.ipa"
+    -F "ipa=@$WORKSPACE/.build/last/default-ios/build.ipa”
 
 #    -F "ipa=@/BUILD_PATH/testfairy.sawsessions.default-ios/.build/last/default-ios/build.ipa"
 #   -F testers_groups='' \
