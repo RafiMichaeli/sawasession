ab6d2a5cfadd52c66f7ecbd7be0299d0829e95d8
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-02-18 10:18:22 +0200

    Update UCB-upload-ipa.sh

--- Assets/Editor/UCB-upload-ipa.sh
+++ Assets/Editor/UCB-upload-ipa.sh
@@ -2,6 +2,8 @@
 
 APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
 
+env
+
 curl -v https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
     -F "file=@$WORKSPACE/.build/last/default-ios/build.ipa"
