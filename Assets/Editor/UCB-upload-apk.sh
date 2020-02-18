d4308a86b669666731face5f286a8f9569017159
RafiMichaeli <35917992+RafiMichaeli@users.noreply.github.com>
2020-02-18 10:01:29 +0200

    Update UCB-upload-apk.sh

--- Assets/Editor/UCB-upload-apk.sh
+++ Assets/Editor/UCB-upload-apk.sh
@@ -2,6 +2,9 @@
 
 APP_TOKEN='2b7c2001126cbbfb221535b6e0d3728f2bf5d9f3'
 
+env
+
+
 curl -v https://upload.testfairy.com/api/upload \
     -F "api_key=${APP_TOKEN}" \
     -F "file=@$WORKSPACE/.build/last/default-android/build.apk"
